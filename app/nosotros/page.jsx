b4ae4815b3098becb81b11718f403fbfdd661a3f
SamuelDvid <samuelrudas90@gmail.com>
2024-11-29 06:59:50 -0500

    Se crea un menu desplegable para la opcion ingresar, asimismo para la opcion: Nuestros aliados

---
+++ app/nosotros/page.jsx
@@ -0,0 +1,26 @@
+import "./page.css";
+
+export default function Nosotros() {
+  return (
+    <>
+      <h1 className="titulo">Sobre Nosotros</h1>
+
+      <article className="contenedor-articulo">
+        <aside>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dignissimos iure veritatis esse dolore, unde recusandae veniam,
+            nobis ducimus obcaecati voluptate? Ratione dolorem, repellat
+            voluptatum illo architecto sit distinctio provident?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dignissimos iure veritatis esse dolore, unde recusandae veniam,
+            nobis ducimus obcaecati voluptate? Ratione dolorem, repellat
+            voluptatum illo architecto sit distinctio provident?
+          </p>
+        </aside>
+      </article>
+    </>
+  );
+}
--- componentes/Navbar.jsx
+++ componentes/Navbar.jsx
@@ -5,24 +5,37 @@ export default function Navbar() {
     <div>
       <nav className="navbar-container">
         <h2>
-          <Link className="navbar" href="/inicio">
+          <div className="dropdown">
             Ingreso
-          </Link>
+            <div className="dropdown-content">
+              <a href="">Usuario</a>
+              <a href="">Eps</a>
+            </div>
+          </div>
         </h2>
+
         <h2>
           <Link className="navbar" href="/nosotros">
             Sobre Nosotros
           </Link>
         </h2>
         <h2>
-          <Link className="navbar" href="/inicio">
+          <div className="dropdown">
             Nuestros Aliados
-          </Link>
+            <div className="dropdown-content">
+              <a href="">Mutual Ser</a>
+              <a href="">Salud Total</a>
+              <a href="">Sanitas</a>
+            </div>
+          </div>
         </h2>
         <h2>
-          <Link className="navbar" href="/inicio">
+          <div className="dropdown">
             Ayuda y Soporte
-          </Link>
+            <div className="dropdown-content">
+              <a href="">Contactanos</a>
+            </div>
+          </div>
         </h2>
       </nav>
     </div>
